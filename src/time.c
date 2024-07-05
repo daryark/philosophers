63027d755e817c9eba1a5dda6528ff8ff8e44684
Dasha Yarkovska <dyarkovs@c2r4s2.42wolfsburg.de>
2024-07-05 02:25:50 +0200

    changed time fn, but works bad

--- src/time.c
+++ src/time.c
@@ -6,7 +6,7 @@
 /*   By: dyarkovs <dyarkovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 22:07:26 by dyarkovs          #+#    #+#             */
-/*   Updated: 2024/07/03 14:52:00 by dyarkovs         ###   ########.fr       */
+/*   Updated: 2024/07/04 16:29:28 by dyarkovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,41 @@ long gettimefromstart_ms(long start_mcs)
     return ((gettimeofday_in_mcs() - start_mcs) / 1000);
 }
 
-bool    monitor_usleep(int mcs, t_philo *philo)
-{
-    long    start;
-    long    now;
+// bool    monitor_usleep(int mcs, t_philo *philo)
+// {
+//     long    start;
+//     long    now;
     
+//     start = gettimeofday_in_mcs();
+//     now = start;
+//     while (now - start < mcs)
+//     {
+//         if (check_dead(philo) || check_full(philo->data))
+//             return (false);
+//         now  = gettimeofday_in_mcs();
+//     }
+//     return (true);
+// }
+
+bool monitor_usleep(int mcs, t_philo *philo)
+{
+    long start;
+    long now;
+    long elapsed;
+
+    (void)philo;
     start = gettimeofday_in_mcs();
-    now = start;
-    while (now - start < mcs)
+    while (true)
     {
-        if (check_dead(philo) || check_full(philo->data))
-            return (false);
-        now  = gettimeofday_in_mcs();
+        now = gettimeofday_in_mcs();
+        elapsed = now - start;
+        if (elapsed >= mcs)
+            break;
+        if (elapsed > 1000)
+            usleep(1000);
+        else
+            usleep(mcs - elapsed);
     }
-    return (true);
+    return true;
 }
+
